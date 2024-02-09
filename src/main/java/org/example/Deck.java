807cf1f791c61c9ae6f67bad93aa507f17908c00
KienTD2005 <kie2127421@maricopa.edu>
2024-02-08 17:35:40 -0700

    For Card Activity Project

---
+++ src/main/java/org/example/Deck.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public interface Deck {
+    void cut(int index);
+    Card deal();
+    void newOrder(Deck cards);
+    int search(Card cards);
+    void shuffle();
+    Card turnOver();
+}
